c016beb178ca7077d41c8fa24108698c4e31d643
C.J. May <maycab@uni.edu>
2018-09-07 19:19:57 -0500

    Consistent Whitespace

--- newuser.py
+++ newuser.py
@@ -7,7 +7,9 @@ Description: Script to add users that can log into bash honeypot from jailshell.
 import bcrypt
 import getpass
 
+
 def main():
+    """Create a new user and store the hashed password in jail.shadow"""
     print("Creating a new user for bash honeypot.")
     username = input("Enter the name of user to be added: ")
     password = ""
@@ -16,8 +18,9 @@ def main():
         password = getpass.getpass("Enter a new password: ")
         pswdCheck = getpass.getpass("Verify new password: ")
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-    with open("jail.shadow","a+") as f:
+    with open("jail.shadow", "a+") as f:
         f.write(username + " " + hashed.decode() + "\n")
     f.close()
+
 
 main()
